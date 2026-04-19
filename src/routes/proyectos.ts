f512f0a3a8e409edea8c84606b9e004a09a329d8
pao243685 <243685@ids.upchiapas.edu.mx>
2026-04-19 10:50:04 -0600

    eliminacion de endpoint para elimiacion de artista y creacion

--- src/routes/proyectos.ts
+++ src/routes/proyectos.ts
@@ -1,18 +1,14 @@
 import { Router } from 'express'
 import {
-  listarProyectos,
   obtenerProyecto,
   crearProyecto,
-  editarProyecto,
   eliminarProyecto
 } from '../controllers/proyectosController'
 
 const router: Router = Router()
 
-router.get('/',       listarProyectos)
 router.get('/:id',    obtenerProyecto)
 router.post('/',      crearProyecto)
-router.patch('/:id',  editarProyecto)
 router.delete('/:id', eliminarProyecto)
 
 export default router
